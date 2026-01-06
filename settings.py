614694aa40c1ce09c319106d6a602e7e28ca7909
Юлия <yulia.designer@mail.ru>
2026-01-06 17:48:29 +0300

    new

--- settings.py
+++ settings.py
@@ -6,3 +6,6 @@ VK_CONFIRMATION = os.getenv("VK_CONFIRMATION", "")
 
 TG_TOKEN = os.getenv("TG_TOKEN", "")
 TG_WEBHOOK_SECRET = os.getenv("TG_WEBHOOK_SECRET", "")
+
+TYPING_DELAY_MIN = float(os.getenv("TYPING_DELAY_MIN", "3"))
+TYPING_DELAY_MAX = float(os.getenv("TYPING_DELAY_MAX", "6"))
